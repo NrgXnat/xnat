487b3557ba6331ef51437aefd14833ecfb83d003
Tim Olsen <olsent@wustl.edu>
2010-04-29 16:15:33 -0500

    Copy from CVS: Fix for lack of TurbineUtils in Context when loaded from Rest code.  Had to override the doBuild method, to insert TurbineUtils like SecureScreen does. BUGZID:1264

--- org/nrg/xdat/turbine/modules/screens/Login.java
+++ org/nrg/xdat/turbine/modules/screens/Login.java
@@ -2,10 +2,22 @@
 package org.nrg.xdat.turbine.modules.screens;
 
 import org.apache.turbine.modules.screens.VelocitySecureScreen;
+import org.apache.turbine.services.velocity.TurbineVelocity;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
+import org.nrg.xdat.turbine.utils.TurbineUtils;
 
 public class Login extends VelocitySecureScreen {
+
+	@Override
+	protected void doBuildTemplate(RunData data) throws Exception {
+		Context c = TurbineVelocity.getContext(data);
+        String systemName = TurbineUtils.GetSystemName();
+        c.put("turbineUtils",TurbineUtils.GetInstance());
+        c.put("systemName",systemName);
+        doBuildTemplate(data, c);
+	}
+
 	@Override
 	protected void doBuildTemplate(RunData data, Context context) throws Exception {
 		for(final Object param : data.getParameters().keySet()){
@@ -15,6 +27,9 @@ public class Login extends VelocitySecureScreen {
 			}
 		}
 	}
+
+
+
 
 	@Override
 	protected boolean isAuthorized(RunData arg0) throws Exception {

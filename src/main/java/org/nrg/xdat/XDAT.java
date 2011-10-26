0d96f597516a9d1d4e44ff429255bfeea2e7c7ad
Rick Herrick <rick.herrick@wustl.edu>
2011-10-26 16:27:09 -0500

    Added XDAT methods for retrieving Spring-based services. Added XftFieldExclusionService and its implementation dependencies.

--- src/main/java/org/nrg/xdat/XDAT.java
+++ src/main/java/org/nrg/xdat/XDAT.java
@@ -1,5 +1,5 @@
 //Copyright 2005 Harvard University / Howard Hughes Medical Institute (HHMI) All Rights Reserved
-/* 
+/*
  * XDAT � Extensible Data Archive Toolkit
  * Copyright (C) 2005 Washington University
  */
@@ -9,50 +9,53 @@
  */
 package org.nrg.xdat;
 
-import java.util.Iterator;
-
 import org.apache.commons.configuration.Configuration;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.stratum.lifecycle.Configurable;
 import org.apache.stratum.lifecycle.Initializable;
-import org.apache.torque.TorqueException;
+import org.nrg.framework.services.ContextService;
+import org.nrg.framework.services.MarshallerCacheService;
+import org.nrg.mail.services.MailService;
+import org.nrg.notify.services.NotificationService;
 import org.nrg.xdat.display.DisplayManager;
 import org.nrg.xdat.security.ElementSecurity;
 import org.nrg.xft.XFT;
 import org.nrg.xft.db.ViewManager;
-import org.nrg.xft.exception.ElementNotFoundException;
-import org.nrg.xft.exception.XFTInitException;
 import org.nrg.xft.generators.SQLCreateGenerator;
 import org.nrg.xft.generators.SQLUpdateGenerator;
 import org.nrg.xft.schema.Wrappers.GenericWrapper.GenericWrapperElement;
+import org.nrg.xft.services.XftFieldExclusionService;
 import org.nrg.xft.utils.FileUtils;
+
 /**
  * @author Tim
  *
  */
 public class XDAT implements Initializable,Configurable{
-	static org.apache.log4j.Logger logger = Logger.getLogger(XDAT.class);
+	static Logger logger = Logger.getLogger(XDAT.class);
+	private static ContextService _contextService;
+	private static MailService _mailService;
+    private static NotificationService _notificationService;
+    private static XftFieldExclusionService _exclusionService;
+    private static MarshallerCacheService _marshallerCacheService;
 	private String instanceSettingsLocation = null;
+
 	/**
 	 * configure torque
 	 *
-	 * @param conf Configuration
+	 * @param configuration Configuration
 	 * @see org.apache.stratum.lifecycle.Configurable
-	 * @throws TorqueException Any exceptions caught during processing will be
-	 *         rethrown wrapped into a TorqueException.
 	 */
-	public void configure(Configuration conf)
+	public void configure(Configuration configuration)
 	{
-		instanceSettingsLocation = conf.getString("instance_settings_directory");
+		instanceSettingsLocation = configuration.getString("instance_settings_directory");
 	}
 
 	/**
 	 * initialize Torque
 	 *
 	 * @see org.apache.stratum.lifecycle.Initializable
-	 * @throws TorqueException Any exceptions caught during processing will be
-	 *         rethrown wrapped into a TorqueException.
 	 */
 	public void initialize()
 	{
@@ -68,18 +71,18 @@ public class XDAT implements Initializable,Configurable{
 	{
 		XDAT.init(location,true,true);
 	}
-	
+
 	public static void RefreshDisplay()
 	{
 		DisplayManager.clean();
 		DisplayManager.GetInstance();
 	}
-	
+
 	public static void init(String location,boolean allowDBAccess) throws Exception
 	{
 		init(location,allowDBAccess,true);
 	}
-	
+
 	public static void init(String location,boolean allowDBAccess, boolean initLog4j) throws Exception
 	{
 		DisplayManager.clean();
@@ -94,19 +97,13 @@ public class XDAT implements Initializable,Configurable{
 		if (allowDBAccess)
 		{
 			try {
-	            Iterator quarantineElements = ElementSecurity.GetQuarantinedElements().iterator();
-	            while (quarantineElements.hasNext())
-	            {
-	                ElementSecurity es = (ElementSecurity)quarantineElements.next();
-	                GenericWrapperElement.GetElement(es.getElementName()).setQuarantineSetting(es.getBooleanProperty("quarantine",false));
-	            }
-	            
-	            Iterator preloadElements = ElementSecurity.GetPreLoadElements().iterator();
-	            while (preloadElements.hasNext())
-	            {
-	                ElementSecurity es = (ElementSecurity)preloadElements.next();
-	                GenericWrapperElement.GetElement(es.getElementName()).setPreLoad(es.getBooleanProperty("pre_load",false));
-	            }
+                for (ElementSecurity es : ElementSecurity.GetQuarantinedElements()) {
+                    GenericWrapperElement.GetElement(es.getElementName()).setQuarantineSetting(es.getBooleanProperty("quarantine", false));
+                }
+
+                for (ElementSecurity es : ElementSecurity.GetPreLoadElements()) {
+                    GenericWrapperElement.GetElement(es.getElementName()).setPreLoad(es.getBooleanProperty("pre_load", false));
+                }
 	        } catch (Exception e) {
 	            logger.error("",e);
 	        }
@@ -117,64 +114,114 @@ public class XDAT implements Initializable,Configurable{
 		DisplayManager.GetInstance();
 		//XFT.LogCurrentTime("XDAT INIT: 3","ERROR");
 	}
-	
-	public static void GenerateUpdateSQL(String file) throws XFTInitException,ElementNotFoundException, Exception
+
+	public static void GenerateUpdateSQL(String file) throws Exception
 	{
-	    StringBuffer sb =new StringBuffer();
-	    sb.append("-- Generated SQL File for updating an existing XNAT database.\n");
-	    sb.append("-- This script is created by the update XNAT feature, which reviews an existing database and only specifies create statements for missing TABLES and COLUMNS.  It will also drop and recreate any necessary functions or views.\n");
-	    
-	    sb.append("-- If you are running from pgAdmin, remove the following line to stop on errors (pgAdmin does not recognize the statement)\n");
-	    sb.append("\\set ON_ERROR_STOP on;\n");
-	    sb.append("\n-- start transaction (if an error occurs, the database will be rolled back to its state before this file was executed)\n");
-	    sb.append("BEGIN;\n");
-	    
-	    Iterator iter = SQLUpdateGenerator.GetSQLCreate().iterator();
-	    while (iter.hasNext())
+        StringBuilder buffer = new StringBuilder();
+	    buffer.append("-- Generated SQL File for updating an existing XNAT database.\n");
+	    buffer.append("-- This script is created by the update XNAT feature, which reviews an existing database and only specifies create statements for missing TABLES and COLUMNS.  It will also drop and recreate any necessary functions or views.\n");
+	    buffer.append("-- If you are running from pgAdmin, remove the following line to stop on errors (pgAdmin does not recognize the statement)\n");
+	    buffer.append("\\set ON_ERROR_STOP on;\n");
+	    buffer.append("\n-- start transaction (if an error occurs, the database will be rolled back to its state before this file was executed)\n");
+	    buffer.append("BEGIN;\n");
+
+	    for (Object item : SQLUpdateGenerator.GetSQLCreate())
 	    {
-	        sb.append(iter.next() +"\n--BR\n");
+	        buffer.append(item).append("\n--BR\n");
 	    }
-		sb.append("\n\n-- REMOVE OLD VIEWS FOR DISPLAY DOCS\n\n");
-		sb.append("\n\nSELECT removeViews();\n--BR\n");
-		sb.append("\n\n-- ADDED VIEWS FOR DISPLAY DOCS\n\n");
-		iter = DisplayManager.GetCreateViewsSQL(true).iterator();
-	    while (iter.hasNext())
+		buffer.append("\n\n-- REMOVE OLD VIEWS FOR DISPLAY DOCS\n\n");
+		buffer.append("\n\nSELECT removeViews();\n--BR\n");
+		buffer.append("\n\n-- ADDED VIEWS FOR DISPLAY DOCS\n\n");
+	    for (Object item : DisplayManager.GetCreateViewsSQL(true))
 	    {
-	        sb.append(iter.next() +"\n--BR\n");
+	        buffer.append(item).append("\n--BR\n");
 	    }
-	    sb.append("\n-- commit transaction\n");
-	    sb.append("COMMIT;");
-		FileUtils.OutputToFile(sb.toString(),file);
-		
+	    buffer.append("\n-- commit transaction\n");
+	    buffer.append("COMMIT;");
+		FileUtils.OutputToFile(buffer.toString(),file);
+
 		logger.info("File Created: " + file);
 	}
-	
-	public static void GenerateCreateSQL(String file) throws XFTInitException,ElementNotFoundException, Exception
+
+	public static void GenerateCreateSQL(String file) throws Exception
 	{
-	    StringBuffer sb =new StringBuffer();
-	    sb.append("-- Generated SQL File for creating an XNAT database from scratch.\n");
-	    
-	    sb.append("-- If you are running from pgAdmin, remove the following line to stop on errors (pgAdmin does not recognize the statement)\n");
-	    sb.append("\\set ON_ERROR_STOP on;\n");
-	    sb.append("\n-- start transaction (if an error occurs, the database will be rolled back to its state before this file was executed)\n");
-	    sb.append("BEGIN;\n");
-	    
-	    Iterator iter = SQLCreateGenerator.GetSQLCreate().iterator();
-	    while (iter.hasNext())
+        StringBuilder buffer = new StringBuilder();
+	    buffer.append("-- Generated SQL File for creating an XNAT database from scratch.\n");
+	    buffer.append("-- If you are running from pgAdmin, remove the following line to stop on errors (pgAdmin does not recognize the statement)\n");
+	    buffer.append("\\set ON_ERROR_STOP on;\n");
+	    buffer.append("\n-- start transaction (if an error occurs, the database will be rolled back to its state before this file was executed)\n");
+	    buffer.append("BEGIN;\n");
+
+	    for (Object item : SQLCreateGenerator.GetSQLCreate())
 	    {
-	        sb.append(iter.next() +"\n--BR\n");
+	        buffer.append(item).append("\n--BR\n");
 	    }
-		sb.append("\n\n-- ADDED VIEWS FOR DISPLAY DOCS\n\n");
-		iter = DisplayManager.GetCreateViewsSQL(false).iterator();
-	    while (iter.hasNext())
+		buffer.append("\n\n-- ADDED VIEWS FOR DISPLAY DOCS\n\n");
+	    for (Object item : DisplayManager.GetCreateViewsSQL(false))
 	    {
-	        sb.append(iter.next() +"\n--BR\n");
+	        buffer.append(item).append("\n--BR\n");
 	    }
-	    sb.append("\n-- commit transaction\n");
-	    sb.append("COMMIT;");
-		FileUtils.OutputToFile(sb.toString(),file);
-		
+	    buffer.append("\n-- commit transaction\n");
+	    buffer.append("COMMIT;");
+		FileUtils.OutputToFile(buffer.toString(),file);
+
 		ViewManager.OutputFieldNames();
 		logger.info("File Created: " + file);
 	}
+
+	/**
+	 * Returns an instance of the currently supported mail service.
+	 * @return An instance of the {@link MailService} service.
+	 */
+    // TODO: Optimally, all of the services would be populated by dependency injection, e.g. Spring or Turbine.
+	public static ContextService getContextService() {
+		if (_contextService == null) {
+		    _contextService = ContextService.getInstance();
+		}
+    	return _contextService;
+	}
+
+	/**
+	 * Returns an instance of the currently supported mail service.
+	 * @return An instance of the {@link MailService} service.
+	 */
+	public static MailService getMailService() {
+	    if (_mailService == null) {
+	        _mailService = getContextService().getBean(MailService.class);
+	    }
+	    return _mailService;
+	}
+
+	/**
+	 * Returns an instance of the currently supported {@link NotificationService notification service}.
+	 * @return An instance of the {@link NotificationService notification service}.
+	 */
+	public static NotificationService getNotificationService() {
+	    if (_notificationService == null) {
+	        _notificationService = getContextService().getBean(NotificationService.class);
+	    }
+	    return _notificationService;
+	}
+	
+	/**
+	 * Returns an instance of the currently supported {@link XftFieldExclusionService exclusion service}.
+	 * @return An instance of the {@link XftFieldExclusionService exclusion service}.
+	 */
+	public static XftFieldExclusionService getExclusionService() {
+	    if (_exclusionService == null) {
+	        _exclusionService = getContextService().getBean(XftFieldExclusionService.class);
+	    }
+	    return _exclusionService;
+	}
+
+    /**
+     * Returns an instance of the currently supported {@link MarshallerCacheService XML marshaler cache service}.
+     * @return An instance of the {@link MarshallerCacheService XML marshaler cache service}.
+     */
+    public static MarshallerCacheService getMarshallerCacheService() {
+        if (_marshallerCacheService == null) {
+            _marshallerCacheService = getContextService().getBean(MarshallerCacheService.class);
+}
+        return _marshallerCacheService;
+    }
 }

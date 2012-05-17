3a076d6eeccca8d2f3e446831a5ce9f20eb5236f
jordan52 <jordan52@gmail.com>
2012-05-17 14:26:14 -0500

    Fixes a bug in xdat that throws a CSRF attempt whenever you try to upload a spreadsheet.

---
+++ src/main/java/org/nrg/xdat/turbine/modules/screens/XDATScreen_uploadCSV4.java
@@ -0,0 +1,14 @@
+package org.nrg.xdat.turbine.modules.screens;
+
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+
+public class XDATScreen_uploadCSV4 extends SecureScreen{
+
+	@Override
+	protected void doBuildTemplate(RunData data, Context context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

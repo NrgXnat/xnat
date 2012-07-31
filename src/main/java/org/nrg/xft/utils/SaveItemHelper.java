f4d240a8c23faeb2f7fc5a76466e2538a6e920d6
Tim Olsen <tim@radiologics.com>
2012-07-31 14:45:13 -0500

    Fixed a few more spots where "" would be just like null.

--- src/main/java/org/nrg/xft/utils/SaveItemHelper.java
+++ src/main/java/org/nrg/xft/utils/SaveItemHelper.java
@@ -234,7 +234,7 @@ public class SaveItemHelper {
 		Authorizer.getInstance().authorizeSave(i.getItem(), user);
 		
 		String id=i.getItem().getPKValueString();
-        if(id==null){
+        if(StringUtils.IsEmpty(id)){
         	id=ID_PLACEHOLDER;
         }
 
@@ -294,7 +294,7 @@ public class SaveItemHelper {
 		}
 		
 		String id=i.getItem().getPKValueString();
-        if(id==null){
+        if(StringUtils.IsEmpty(id)){
         	id=ID_PLACEHOLDER;
         }
 
@@ -429,7 +429,7 @@ public class SaveItemHelper {
 		}
 		
 		String id=i.getItem().getPKValueString();
-        if(id==null){
+        if(StringUtils.IsEmpty(id)){
         	id=ID_PLACEHOLDER;
         }
 

d60fa176e3b634a871492db946251ee1ccd38301
Kevin A. Archie <karchie@wustl.edu>
2012-08-23 14:52:33 -0500

    added MaximumValueAttrDef

--- src/test/java/org/nrg/attr/MaximumValueAttrDefTest.java
+++ src/test/java/org/nrg/attr/MaximumValueAttrDefTest.java
@@ -18,46 +18,46 @@ import com.google.common.collect.Lists;
  * @author Kevin A. Archie <karchie@wustl.edu>
  *
  */
-public class MinimumValueAttrDefTest {
+public class MaximumValueAttrDefTest {
     private final EvaluableAttrDef<String,?,Integer> base = new IntegerExtAttrDef<String>("attr", "key");
     
     /**
-     * Test method for {@link org.nrg.attr.MinimumValueAttrDef#wrap(org.nrg.attr.EvaluableAttrDef)}.
+     * Test method for {@link org.nrg.attr.MaximumValueAttrDef#wrap(org.nrg.attr.EvaluableAttrDef)}.
      */
     @Test
     public void testWrap() {
-        final MinimumValueAttrDef<String,?,Integer> min = MinimumValueAttrDef.wrap(base);
+        final MaximumValueAttrDef<String,?,Integer> min = MaximumValueAttrDef.wrap(base);
         assertTrue(min instanceof ExtAttrDef<?>);
     }
 
     /**
-     * Test method for {@link org.nrg.attr.MinimumValueAttrDef#start()}.
+     * Test method for {@link org.nrg.attr.MaximumValueAttrDef#start()}.
      */
     @Test
     public void testStart() {
-        final MinimumValueAttrDef<String,?,Integer> min = MinimumValueAttrDef.wrap(base);
+        final MaximumValueAttrDef<String,?,Integer> min = MaximumValueAttrDef.wrap(base);
         assertEquals(base.start(), min.start());
     }
 
     /**
-     * Test method for {@link org.nrg.attr.MinimumValueAttrDef#foldl(java.lang.Comparable, java.util.Map)}.
+     * Test method for {@link org.nrg.attr.MaximumValueAttrDef#foldl(java.lang.Comparable, java.util.Map)}.
      */
     @Test
     public void testFoldl() throws ExtAttrException {
         @SuppressWarnings("unchecked")
-        final MinimumValueAttrDef<String,Integer,Integer> min = (MinimumValueAttrDef<String,Integer,Integer>)MinimumValueAttrDef.wrap(base);
+        final MaximumValueAttrDef<String,Integer,Integer> min = (MaximumValueAttrDef<String,Integer,Integer>)MaximumValueAttrDef.wrap(base);
         assertNull(min.foldl(null, Collections.singletonMap("nokey", 3)));
         assertEquals(Integer.valueOf(3), min.foldl(null, Collections.singletonMap("key", 3)));
-        assertEquals(Integer.valueOf(3), min.foldl(3, Collections.singletonMap("key", 4)));
-        assertEquals(Integer.valueOf(2), min.foldl(3, Collections.singletonMap("key", 2)));
+        assertEquals(Integer.valueOf(3), min.foldl(3, Collections.singletonMap("key", 2)));
+        assertEquals(Integer.valueOf(4), min.foldl(3, Collections.singletonMap("key", 4)));
     }
 
     /**
-     * Test method for {@link org.nrg.attr.MinimumValueAttrDef#apply(java.lang.Comparable)}.
+     * Test method for {@link org.nrg.attr.MaximumValueAttrDef#apply(java.lang.Comparable)}.
      */
     @Test
     public void testApplyA() throws ExtAttrException {
-        final MinimumValueAttrDef<String,?,Integer> min = MinimumValueAttrDef.wrap(base);
+        final MaximumValueAttrDef<String,?,Integer> min = MaximumValueAttrDef.wrap(base);
         final List<ExtAttrValue> vs = Lists.newArrayList(min.apply(-2));   
         assertEquals(1, vs.size());
         assertEquals(Integer.valueOf(-2), Integer.valueOf(vs.get(0).getText()));

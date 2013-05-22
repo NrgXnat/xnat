cdd2e056dac6974c896a17ffcf47870e0aaaf948
Rick Herrick <rick.herrick@wustl.edu>
2013-05-22 09:42:06 -0500

    Added processing of user groups into GrantedAuthorities.

--- src/main/java/org/nrg/xdat/entities/XDATUserDetails.java
+++ src/main/java/org/nrg/xdat/entities/XDATUserDetails.java
@@ -1,9 +1,11 @@
 package org.nrg.xdat.entities;
 
+import com.google.common.base.Joiner;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nrg.xdat.security.UserGroup;
 import org.nrg.xdat.security.XDATUser;
 import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.DisabledException;
@@ -11,43 +13,61 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.nrg.xdat.XDAT;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.util.*;
 
 @SuppressWarnings("unchecked")
 public class XDATUserDetails extends XDATUser implements UserDetails {
 
     public XDATUserDetails() {
-        //
+        _log.debug("Called default constructor");
     }
 
     public XDATUserDetails(String user) throws Exception {
         super(user);
+        if (_log.isDebugEnabled()) {
+            _log.debug("Called constructor with string arg: " + user);
+        }
     }
 
     public XDATUserDetails(XDATUser user) throws Exception {
         super(user);
+        if (_log.isDebugEnabled()) {
+            _log.debug("Called constructor with XDATUser arg: " + user.getLogin());
+        }
     }
 
     public XdatUserAuth getAuthorization() {
-        return authorization;
+        return _authorization;
     }
 
     public XdatUserAuth setAuthorization(XdatUserAuth auth) {
-        return authorization = auth;
+        return _authorization = auth;
     }
 
     public Collection<GrantedAuthority> getAuthorities() {
-        Set<GrantedAuthority> list = new HashSet<GrantedAuthority>();
-        list.add(new GrantedAuthorityImpl("ROLE_USER"));
-
-        return list;
+        if (_authorities == null) {
+            _authorities = new ArrayList<GrantedAuthority>();
+            _authorities.add(new GrantedAuthorityImpl("ROLE_USER"));
+            if (isSiteAdmin()) {
+                _authorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
+            }
+            Map<String,UserGroup> groups = getGroups();
+            if (groups != null && groups.size() > 0) {
+                for (String group : groups.keySet()) {
+                    _authorities.add(new GrantedAuthorityImpl(group));
+                }
+            }
+            if (_log.isDebugEnabled()) {
+                _log.debug("Created granted authorities list for user " + getLogin() + ": " + Joiner.on(", ").join(_authorities));
+            }
+        }
+        return _authorities;
     }
 
     public String getPassword() {
         try {
-            return this.getStringProperty("primary_password");
+            return getStringProperty("primary_password");
         } catch (Exception e) {
             return null;
         }
@@ -84,29 +104,26 @@ public class XDATUserDetails extends XDATUser implements UserDetails {
     
     //necessary for spring security session management
 	public int hashCode() {
-        return new HashCodeBuilder(691, 431). // two randomly chosen prime numbers
-            // if deriving: appendSuper(super.hashCode()).
-        	append(this.getUsername()).
-            
-            toHashCode();
+        return new HashCodeBuilder(691, 431).append(getUsername()).toHashCode();
     }
 
 	public boolean equals(Object obj) {
-        if (obj == null)
+        if (obj == null) {
             return false;
-        if (obj == this)
+        }
+        if (obj == this) {
             return true;
-        if (obj.getClass() != getClass())
+        }
+        if (obj.getClass() != getClass()) {
             return false;
+        }
 
         XDATUserDetails rhs = (XDATUserDetails) obj;
-        return new EqualsBuilder().
-            // if deriving: appendSuper(super.equals(obj)).
-        	append(this.getUsername(), rhs.getUsername()).
-            isEquals();
+        return new EqualsBuilder().append(getUsername(), rhs.getUsername()).isEquals();
     }
 
     private static final Log _log = LogFactory.getLog(XDATUserDetails.class);
 
-    private XdatUserAuth authorization;
+    private XdatUserAuth _authorization;
+    private List<GrantedAuthority> _authorities;
 }

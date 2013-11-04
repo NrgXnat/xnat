8db2f15a90a208edafa8a7fa9d4741de81edb857
Justin Cleveland <clevelandj@mir.wustl.edu>
2013-11-04 16:56:34 -0600

    XNAT-2114 - Email Disorder (now ordering by firstname, then last)

--- src/main/java/org/nrg/xdat/turbine/modules/screens/XDATScreen_email.java
+++ src/main/java/org/nrg/xdat/turbine/modules/screens/XDATScreen_email.java
@@ -6,10 +6,14 @@
 package org.nrg.xdat.turbine.modules.screens;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.turbine.utils.TurbineUtils;
+import org.nrg.xft.XFTItem;
 import org.nrg.xft.search.ItemSearch;
 
 /**
@@ -24,10 +28,23 @@ public class XDATScreen_email extends SecureScreen {
     protected void doBuildTemplate(RunData data, Context context)
             throws Exception {
         ArrayList al = ItemSearch.GetAllItems("xdat:user",null,false).items();
+        Collections.sort(al, new nameComparator());
         context.put("users",al);
         
         String s= (String) TurbineUtils.GetPassedParameter("emailTo",data,"");
         context.put("emailTo",s);
     }
-
+    public class nameComparator implements Comparator<XFTItem> {
+        public int compare(XFTItem object1, XFTItem object2) {
+            String fn1 = (String) object1.getField("firstname");
+            String ln1 = (String) object1.getField("lastname");
+            String fn2 = (String) object2.getField("firstname");
+            String ln2 = (String) object2.getField("lastname");
+            int c = fn1.compareTo(fn2);
+            if(c!=0){
+                return c;
+            }
+            return ln1.compareTo(ln2);
+        }
+    }
 }

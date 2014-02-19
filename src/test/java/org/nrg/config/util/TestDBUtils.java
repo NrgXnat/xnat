80d8173341f55438b4e6695fad6d72ea205f1f8f
Rick Herrick <rick.herrick@wustl.edu>
2014-02-19 08:46:06 -0600

    XNAT-2920: Updated copyright notice, prepared for 1.6.3 tag.

--- src/test/java/org/nrg/config/util/TestDBUtils.java
+++ src/test/java/org/nrg/config/util/TestDBUtils.java
@@ -1,12 +1,12 @@
 /*
  * org.nrg.config.util.TestDBUtils
  * XNAT http://www.xnat.org
- * Copyright (c) 2013, Washington University School of Medicine
+ * Copyright (c) 2014, Washington University School of Medicine
  * All Rights Reserved
  *
  * Released under the Simplified BSD.
  *
- * Last modified 7/1/13 5:30 PM
+ * Last modified 9/23/13 8:28 AM
  */
 package org.nrg.config.util;
 

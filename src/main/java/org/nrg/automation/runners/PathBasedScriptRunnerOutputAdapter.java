8c9852c17f67fe02eb0f4038af9c5ddd3a1863ab
Rick Herrick <jrherrick@wustl.edu>
2015-12-18 12:32:58 -0600

    Added initialize() method along with default constructor to allow for lazy initialization.

--- src/main/java/org/nrg/automation/runners/PathBasedScriptRunnerOutputAdapter.java
+++ src/main/java/org/nrg/automation/runners/PathBasedScriptRunnerOutputAdapter.java
@@ -20,21 +20,12 @@ import java.util.Map;
 
 public class PathBasedScriptRunnerOutputAdapter implements ScriptRunnerOutputAdapter {
 
+    public PathBasedScriptRunnerOutputAdapter() throws NrgServiceException {
+        // Nothing to do here.
+    }
+
     public PathBasedScriptRunnerOutputAdapter(final String path) throws NrgServiceException {
-        if (StringUtils.isNotBlank(path)) {
-            if (_log.isDebugEnabled()) {
-                _log.debug("Initializing the path-based script runner output adapter with the path: " + path);
-            }
-            setPath(path);
-        } else {
-            if (_log.isDebugEnabled()) {
-                _log.debug("Initializing the path-based script runner output adapter without a path specified. Using temp path.");
-            }
-            final Path tmpdir = Paths.get(System.getProperty("java.io.tmpdir"));
-            final Path scripts = tmpdir.resolve("scripts");
-            scripts.toFile().mkdir();
-            setPath(scripts.toString());
-        }
+        initialize(path);
     }
 
     @Override
@@ -49,6 +40,10 @@ public class PathBasedScriptRunnerOutputAdapter implements ScriptRunnerOutputAda
         }
     }
 
+    public boolean hasPath() {
+        return _path != null;
+    }
+
     public void setPath(final String path) {
         if (StringUtils.isBlank(path) || (_path != null &&  Paths.get(path).compareTo(_path) == 0)) {
             return;
@@ -60,9 +55,28 @@ public class PathBasedScriptRunnerOutputAdapter implements ScriptRunnerOutputAda
         _folders.clear();
     }
 
+    protected void initialize(String path) {
+        if (StringUtils.isNotBlank(path)) {
+            if (_log.isDebugEnabled()) {
+                _log.debug("Initializing the path-based script runner output adapter with the path: " + path);
+            }
+            setPath(path);
+        } else {
+            if (_log.isDebugEnabled()) {
+                _log.debug("Initializing the path-based script runner output adapter without a path specified. Using temp path.");
+            }
+            final Path tmpdir = Paths.get(System.getProperty("java.io.tmpdir"));
+            final Path scripts = tmpdir.resolve("scripts");
+            //noinspection ResultOfMethodCallIgnored
+            scripts.toFile().mkdir();
+            setPath(scripts.toString());
+        }
+    }
+
     private Path getScriptFolder(final String scriptId) {
         final Path scriptFolder = _path.resolve(scriptId);
         if (!scriptFolder.toFile().exists()) {
+            //noinspection ResultOfMethodCallIgnored
             scriptFolder.toFile().mkdirs();
         } else if (!scriptFolder.toFile().isDirectory()) {
             throw new NrgServiceRuntimeException(NrgServiceError.Unknown, "Can't write to the path " + scriptFolder + ": it already exists but isn't a directory.");
@@ -73,5 +87,5 @@ public class PathBasedScriptRunnerOutputAdapter implements ScriptRunnerOutputAda
 
     private static final Logger _log = LoggerFactory.getLogger(PathBasedScriptRunnerOutputAdapter.class);
     private Path _path;
-    private final Map<String, Path> _folders = new HashMap<String, Path>();
+    private final Map<String, Path> _folders = new HashMap<>();
 }

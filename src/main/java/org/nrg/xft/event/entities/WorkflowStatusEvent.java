ab16f75c9c27542512850c571f90978250f8cffc
Mike Hodge <hodgem@wustl.edu>
2016-07-15 12:21:57 -0500

    XNAT-4416, Event handler filters should have a required option for filterable values

--- src/main/java/org/nrg/xft/event/entities/WorkflowStatusEvent.java
+++ src/main/java/org/nrg/xft/event/entities/WorkflowStatusEvent.java
@@ -90,7 +90,7 @@ public class WorkflowStatusEvent extends AutomationEvent {
 	 *
 	 * @return the status
 	 */
-	@Filterable(initialValues = { "Complete", "Failed" }, includeValuesFromDatabase = false)
+	@Filterable(initialValues = { "Complete", "Failed" }, filterRequired = true, includeValuesFromDatabase = false)
 	public String getStatus() {
 		return status;
 	}

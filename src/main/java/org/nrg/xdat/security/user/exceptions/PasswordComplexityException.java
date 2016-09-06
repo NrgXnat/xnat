8403b6be480d2e7bb9290ac61263f2d1f0fb4617
Rick Herrick <jrherrick@wustl.edu>
2016-09-06 16:15:48 -0500

    Added response status to password complexity exception.

--- src/main/java/org/nrg/xdat/security/user/exceptions/PasswordComplexityException.java
+++ src/main/java/org/nrg/xdat/security/user/exceptions/PasswordComplexityException.java
@@ -1,5 +1,9 @@
 package org.nrg.xdat.security.user.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "The specified password did not meet the complexity requirements configured for the site.")
 public class PasswordComplexityException extends Exception {
     public PasswordComplexityException(String message) {
         super(message);

65b4ddfbb0e7b41fe77f6ac817960f9f472e7f96
Rick Herrick <rherrick@gmail.com>
2016-09-20 11:19:25 -0500

    1.7.0 release

--- src/main/java/org/nrg/transaction/RollbackException.java
+++ src/main/java/org/nrg/transaction/RollbackException.java
@@ -1,12 +1,10 @@
 /*
  * org.nrg.transaction.RollbackException
  * XNAT http://www.xnat.org
- * Copyright (c) 2014, Washington University School of Medicine
+ * Copyright (c) 2016, Washington University School of Medicine
  * All Rights Reserved
  *
  * Released under the Simplified BSD.
- *
- * Last modified 7/1/13 5:31 PM
  */
 package org.nrg.transaction;
 

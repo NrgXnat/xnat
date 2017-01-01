e4bf36148eba0f98bf16832ae37fdc0d250a8656
Rick Herrick <jrherrick@wustl.edu>
2016-12-31 18:09:34 -0600

    XNAT-4607 Updated to 1.7.2-SNAPSHOT and fixed issues with modify password that were breaking Selenium tests.

--- src/main/java/org/nrg/xdat/turbine/modules/actions/ModifyEmail.java
+++ src/main/java/org/nrg/xdat/turbine/modules/actions/ModifyEmail.java
@@ -7,13 +7,12 @@
  * Released under the Simplified BSD.
  */
 
-
 package org.nrg.xdat.turbine.modules.actions;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
+import org.nrg.xdat.XDAT;
 import org.nrg.xdat.security.ElementSecurity;
 import org.nrg.xdat.security.helpers.Users;
 import org.nrg.xdat.security.user.exceptions.UserFieldMappingException;
@@ -23,101 +22,92 @@ import org.nrg.xft.event.EventUtils;
 import org.nrg.xft.exception.InvalidPermissionException;
 import org.nrg.xft.security.UserI;
 import org.nrg.xft.utils.ValidationUtils.ValidationResultsI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("unused")
 public class ModifyEmail extends SecureAction {
-
-    static Logger logger = Logger.getLogger(ModifyEmail.class);
-    public void doPerform(RunData data, Context context) throws Exception
-    {
-    	UserI found;
-    	try {
-			found= Users.createUser(TurbineUtils.GetDataParameterHash(data));
-		} catch (UserFieldMappingException e1) {
+    public void doPerform(final RunData data, final Context context) throws Exception {
+        final UserI found;
+        try {
+            found = Users.createUser(TurbineUtils.GetDataParameterHash(data));
+        } catch (UserFieldMappingException e1) {
             data.addMessage(e1.getMessage());
-            if (((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)) !=null)
-            {
-                data.setScreenTemplate(((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)));
+            if (TurbineUtils.GetPassedParameter("edit_screen", data) != null) {
+                data.setScreenTemplate(((String) TurbineUtils.GetPassedParameter("edit_screen", data)));
             }
             return;
-		}
-		
-		UserI existing=null;
-		if(found.getID()!=null){
-			existing=Users.getUser(found.getID());
-		}
-		
-		if(existing==null && found.getLogin()!=null){
-			existing=Users.getUser(found.getLogin());
-		}
-		
-		if(existing==null){
-			data.addMessage("Unable to identify user for email modification.");
-            if (((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)) !=null)
-            {
-                data.setScreenTemplate(((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)));
-            }
-            return;
-		}
-		
-		String oldEmail=existing.getEmail();
-		if(found.getEmail()==null || StringUtils.equals(oldEmail, found.getEmail())){
-			data.addMessage("Email address unchanged.");
-            if (((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)) !=null)
-            {
-                data.setScreenTemplate(((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)));
-            }
-            return;
-		}
-		
-		existing.setEmail(found.getEmail());
+        }
 
-		ValidationResultsI vr =Users.validate(existing);
-        
-		if(!vr.isValid()){
-			TurbineUtils.SetEditItem(found,data);
-            context.put("vr",vr);
-            if (((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)) !=null)
-            {
-                data.setScreenTemplate(((String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("edit_screen",data)));
+        UserI existing = null;
+        if (found.getID() != null) {
+            existing = Users.getUser(found.getID());
+        }
+
+        if (existing == null && found.getLogin() != null) {
+            existing = Users.getUser(found.getLogin());
+        }
+
+        if (existing == null) {
+            data.addMessage("Unable to identify user for email modification.");
+            if (TurbineUtils.GetPassedParameter("edit_screen", data) != null) {
+                data.setScreenTemplate(((String) TurbineUtils.GetPassedParameter("edit_screen", data)));
             }
             return;
-		}
-       
-                
-        UserI authenticatedUser=(UserI)TurbineUtils.getUser(data);
-        
-        try {                	
-        	if(existing.getEmail()!=null){
-        		String newemail = existing.getEmail();
-        		if(!newemail.contains("@")){
-        			data.setMessage("Please enter a valid email address.");
-        			data.setScreenTemplate("XDATScreen_UpdateUser.vm");
-        			return;
-        		}
-        		Users.save(existing, authenticatedUser, false, EventUtils.newEventInstance(EventUtils.CATEGORY.SIDE_ADMIN, EventUtils.TYPE.WEB_FORM, "Modified User Email"));
-            	
-        		UserI item = TurbineUtils.getUser(data);
-        		item.setEmail(existing.getEmail());
-            	AdminUtils.sendUserHTMLEmail("Email address changed.", "Your email address was successfully changed to "+existing.getEmail() + ".", true, new String[]{authenticatedUser.getEmail(),existing.getEmail()});
-            	data.setMessage("Email address changed.");
-        	}else{
-                data.setMessage("Email address unchanged.");
-                data.setScreenTemplate("Index.vm");
-        	}
-        
+        }
+
+        final String oldEmail = existing.getEmail();
+        final String newEmail = found.getEmail();
+
+        if (StringUtils.isBlank(newEmail) || StringUtils.equals(oldEmail, newEmail)) {
+            data.addMessage("Email address unchanged.");
+            if (TurbineUtils.GetPassedParameter("edit_screen", data) != null) {
+                data.setScreenTemplate(((String) TurbineUtils.GetPassedParameter("edit_screen", data)));
+            }
+            return;
+        }
+
+        if (!newEmail.contains("@")) {
+            data.setMessage("Please enter a valid email address.");
+            data.setScreenTemplate("XDATScreen_UpdateUser.vm");
+            return;
+        }
+
+        existing.setEmail(newEmail);
+
+        final ValidationResultsI validation = Users.validate(existing);
+
+        if (!validation.isValid()) {
+            TurbineUtils.SetEditItem(found, data);
+            context.put("vr", validation);
+            if (TurbineUtils.GetPassedParameter("edit_screen", data) != null) {
+                data.setScreenTemplate(((String) TurbineUtils.GetPassedParameter("edit_screen", data)));
+            }
+            return;
+        }
+
+        final UserI user = XDAT.getUserDetails();
+        assert user != null;
+
+        try {
+            Users.save(existing, user, false, EventUtils.newEventInstance(EventUtils.CATEGORY.SIDE_ADMIN, EventUtils.TYPE.WEB_FORM, "Modified User Email"));
+            ElementSecurity.refresh();
+
+            // Update the email address for the user principal in the application session.
+            user.setEmail(existing.getEmail());
+
+            AdminUtils.sendUserHTMLEmail("Email address changed.", "Your email address was successfully changed to " + existing.getEmail() + ".", true, new String[]{user.getEmail(), existing.getEmail()});
+
+            context.put("success", true);
+            data.setMessage("Email address changed.");
+            // TODO: This should be included so that it stays on the update user page rather than going to the main page.
+            // data.setScreenTemplate("XDATScreen_UpdateUser.vm");
         } catch (InvalidPermissionException e) {
-			notifyAdmin(authenticatedUser, data,403,"Possible Authorization Bypass event", "User attempted to modify a user account other then his/her own.  This typically requires tampering with the HTTP form submission process.");
-			return;
-		} catch (Exception e) {
-			logger.error("Error Storing User", e);
-			return;
-		}
-                
-         
-        ElementSecurity.refresh();
-            
-        data.setScreenTemplate("Index.vm");
-
+            notifyAdmin(user, data, 403, "Possible Authorization Bypass event", "User attempted to modify a user account other then his/her own.  This typically requires tampering with the HTTP form submission process.");
+        } catch (Exception e) {
+            logger.error("Error Storing User", e);
+        }
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(ModifyEmail.class);
 }
--- src/main/java/org/nrg/xdat/turbine/modules/actions/ModifyPassword.java
+++ src/main/java/org/nrg/xdat/turbine/modules/actions/ModifyPassword.java
@@ -10,12 +10,14 @@
 package org.nrg.xdat.turbine.modules.actions;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.turbine.Turbine;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.XDAT;
 import org.nrg.xdat.entities.XdatUserAuth;
 import org.nrg.xdat.schema.SchemaElement;
 import org.nrg.xdat.security.ElementSecurity;
+import org.nrg.xdat.security.helpers.Roles;
 import org.nrg.xdat.security.helpers.Users;
 import org.nrg.xdat.security.user.exceptions.PasswordComplexityException;
 import org.nrg.xdat.security.user.exceptions.UserFieldMappingException;
@@ -30,7 +32,6 @@ import org.slf4j.LoggerFactory;
 
 /**
  * @author Tim
- *
  */
 @SuppressWarnings("unused")
 public class ModifyPassword extends SecureAction {
@@ -64,8 +65,9 @@ public class ModifyPassword extends SecureAction {
             existing = Users.getUser(found.getID());
         }
 
-        if (existing == null && found.getLogin() != null) {
-            existing = Users.getUser(found.getLogin());
+        final String login = found.getLogin();
+        if (existing == null && login != null) {
+            existing = Users.getUser(login);
         }
 
         if (existing == null) {
@@ -74,27 +76,40 @@ public class ModifyPassword extends SecureAction {
             return;
         }
 
-        final String newPassword     = data.getParameters().getString("xdat:user.primary_password"); // the object in found will have run the password through escape character encoding, potentially altering it
-        final String oldPassword     = existing.getPassword();
+        final String encodedPassword = existing.getPassword();
         final String currentPassword = data.getParameters().getString("current_password");
+        final String updatedPassword = data.getParameters().getString("xdat:user.primary_password"); // the object in found will have run the password through escape character encoding, potentially altering it
 
-        if ((StringUtils.isBlank(oldPassword) || StringUtils.isBlank(currentPassword) || Users.isPasswordValid(oldPassword, currentPassword, existing.getSalt())) && data.getSession().getAttribute("forgot") == null) {
-            //User correctly entered their old password or they forgot their old password
-            data.setMessage("Incorrect current password. Password unchanged.");
-            redirect(data, false);
-            return;
+        if (data.getSession().getAttribute("forgot") == null) {
+            if (StringUtils.isBlank(encodedPassword) && !Roles.isSiteAdmin(user)) {
+                data.setMessage("The user " + login + " is configured as a no-login user and can't have a password set. If you feel this is an error, please contact your site administrator.");
+                redirect(data, false);
+                return;
+            }
+
+            if (StringUtils.isBlank(currentPassword) || StringUtils.isBlank(updatedPassword) || !Users.isPasswordValid(encodedPassword, currentPassword, existing.getSalt())) {
+                //User correctly entered their old password or they forgot their old password
+                final StringBuilder message = new StringBuilder("Your password was not updated: ");
+                if (StringUtils.isBlank(currentPassword) || StringUtils.isBlank(updatedPassword)) {
+                    message.append("you must provide values for both your current and updated password.");
+                } else if (!Users.isPasswordValid(encodedPassword, currentPassword, existing.getSalt())) {
+                    message.append("you entered an incorrect value for your current password.");
+                }
+                data.setMessage(message.toString());
+                redirect(data, false);
+                return;
+            }
         }
 
-        existing.setPassword(newPassword);
+        existing.setPassword(updatedPassword);
 
         if (!Users.validate(existing).isValid()) {
             redirect(data, false);
         }
 
-        final UserI authenticatedUser = XDAT.getUserDetails();
         try {
-            if (StringUtils.isNotEmpty(newPassword)) {
-                Users.save(existing, authenticatedUser, false, EventUtils.newEventInstance(EventUtils.CATEGORY.SIDE_ADMIN, EventUtils.TYPE.WEB_FORM, "Modified User Password"));
+            if (StringUtils.isNotEmpty(updatedPassword)) {
+                Users.save(existing, user, false, EventUtils.newEventInstance(EventUtils.CATEGORY.SIDE_ADMIN, EventUtils.TYPE.WEB_FORM, "Modified User Password"));
 
                 //need to update password expiration
                 final XdatUserAuth auth = XDAT.getXdatUserAuthService().getUserByNameAndAuth(existing.getUsername(), XdatUserAuthService.LOCALDB, "");
@@ -107,6 +122,7 @@ public class ModifyPassword extends SecureAction {
                     ElementSecurity.refresh();
                 }
 
+                context.put("success", true);
                 data.setMessage("Password changed.");
                 redirect(data, true);
             } else {
@@ -114,7 +130,7 @@ public class ModifyPassword extends SecureAction {
                 redirect(data, false);
             }
         } catch (InvalidPermissionException e) {
-            notifyAdmin(authenticatedUser, data, 403, "Possible Authorization Bypass event", "User attempted to modify a user account other then his/her own.  This typically requires tampering with the HTTP form submission process.");
+            notifyAdmin(user, data, 403, "Possible Authorization Bypass event", "User attempted to modify a user account other then his/her own.  This typically requires tampering with the HTTP form submission process.");
         } catch (PasswordComplexityException e) {
             data.setMessage(e.getMessage());
             redirect(data, false);
@@ -123,34 +139,34 @@ public class ModifyPassword extends SecureAction {
         }
     }
 
-    private void redirect(RunData data, boolean changed){
-        if(changed) {
-            Boolean expired = (Boolean) data.getSession().getAttribute("expired");
-            Boolean forgot = (Boolean) data.getSession().getAttribute("forgot");
-            String loginTemplate = org.apache.turbine.Turbine.getConfiguration().getString("template.login");
-            String homepageTemplate = org.apache.turbine.Turbine.getConfiguration().getString("template.homepage");
-            if ((forgot != null && forgot)) {
+    private void redirect(RunData data, boolean changed) {
+        if (changed) {
+            final Boolean expired          = (Boolean) data.getSession().getAttribute("expired");
+            final Boolean forgot           = (Boolean) data.getSession().getAttribute("forgot");
+            final String  loginTemplate    = Turbine.getConfiguration().getString("template.login");
+            final String  homepageTemplate = Turbine.getConfiguration().getString("template.homepage");
+
+            if (forgot != null && forgot) {
                 //User forgot their password. They must log in again.
                 if (StringUtils.isNotEmpty(loginTemplate)) {
                     // We're running in a templating solution
                     data.setScreenTemplate(loginTemplate);
                 } else {
-                    data.setScreen(org.apache.turbine.Turbine.getConfiguration().getString("screen.login"));
+                    data.setScreen(Turbine.getConfiguration().getString("screen.login"));
                 }
-            }else if ((expired != null && expired)) {
+            } else if (expired != null && expired) {
                 //They just updated expired password.
                 data.getSession().setAttribute("expired", Boolean.FALSE);//New password is not expired
                 if (StringUtils.isNotEmpty(homepageTemplate)) {
                     // We're running in a templating solution
                     data.setScreenTemplate(homepageTemplate);
                 } else {
-                    data.setScreen(org.apache.turbine.Turbine.getConfiguration().getString("screen.homepage"));
+                    data.setScreen(Turbine.getConfiguration().getString("screen.homepage"));
                 }
             } else {
                 data.setScreenTemplate("XDATScreen_UpdateUser.vm");
             }
-        }
-        else{
+        } else {
             data.setScreenTemplate("XDATScreen_UpdateUser.vm");
         }
     }

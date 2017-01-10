df8532977901c0b93de98e0eab19e936236af4af
Rick Herrick <jrherrick@wustl.edu>
2017-01-09 18:37:46 -0600

    1.7.2 release

--- src/main/java/org/nrg/transaction/Run.java
+++ src/main/java/org/nrg/transaction/Run.java
@@ -1,11 +1,12 @@
 /*
- * org.nrg.transaction.Run
+ * transaction: org.nrg.transaction.Run
  * XNAT http://www.xnat.org
- * Copyright (c) 2016, Washington University School of Medicine
+ * Copyright (c) 2017, Washington University School of Medicine
  * All Rights Reserved
  *
  * Released under the Simplified BSD.
  */
+
 package org.nrg.transaction;
 
 
--- src/test/java/org/nrg/transaction/help/RunTest.java
+++ src/test/java/org/nrg/transaction/help/RunTest.java
@@ -1,11 +1,12 @@
 /*
- * org.nrg.transaction.help.RunTest
+ * transaction: org.nrg.transaction.help.RunTest
  * XNAT http://www.xnat.org
- * Copyright (c) 2016, Washington University School of Medicine
+ * Copyright (c) 2017, Washington University School of Medicine
  * All Rights Reserved
  *
  * Released under the Simplified BSD.
  */
+
 package org.nrg.transaction.help;
 
 import static org.junit.Assert.assertEquals;

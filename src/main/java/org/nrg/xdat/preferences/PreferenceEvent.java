5d621dcd04d971eaf8eb2a054597a033193939d7
Rick Herrick <jrherrick@wustl.edu>
2017-09-05 01:19:33 -0500

    XNAT-4873 Added some events including create and delete event triggering.

--- src/main/java/org/nrg/xdat/preferences/PreferenceEvent.java
+++ src/main/java/org/nrg/xdat/preferences/PreferenceEvent.java
@@ -10,11 +10,28 @@
 package org.nrg.xdat.preferences;
 
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.StringUtils;
+import org.nrg.framework.constants.Scope;
 import org.nrg.framework.event.StructuredEvent;
 import org.nrg.framework.event.entities.EventSpecificFields;
+import org.nrg.framework.scope.EntityId;
 
 public class PreferenceEvent extends StructuredEvent {
     public PreferenceEvent(final String preferenceName, final String value) {
+        this(null, null, preferenceName, value);
+    }
+
+    public PreferenceEvent(final EntityId entityId, final String preferenceName, final String value) {
+        this(entityId.getScope(), entityId.getEntityId(), preferenceName, value);
+    }
+
+    public PreferenceEvent(final Scope scope, final String entityId, final String preferenceName, final String value) {
+        if (scope != null) {
+            setEntityType(scope.code());
+        }
+        if (entityId != null) {
+            setEntityId(entityId);
+        }
         setEventSpecificFields(Sets.newHashSet(new EventSpecificFields(preferenceName, value)));
     }
 }

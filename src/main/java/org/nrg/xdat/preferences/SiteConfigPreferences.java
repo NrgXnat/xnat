1bcb005b51173b74a7a429e62ea8391a3f3cd5a5
James Ransford <ransfordj@radiologics.com>
2019-07-17 10:59:19 -0500

    Removing uiShowCachedFileCounts from SiteConfigPreferences.java

--- src/main/java/org/nrg/xdat/preferences/SiteConfigPreferences.java
+++ src/main/java/org/nrg/xdat/preferences/SiteConfigPreferences.java
@@ -1526,19 +1526,6 @@ public class SiteConfigPreferences extends EventTriggeringAbstractPreferenceBean
     }
     
     @NrgPreference(defaultValue = "true")
-    public boolean getUiShowCachedFileCounts() {
-        return getBooleanValue("uiShowCachedFileCounts");
-    }
-
-    public void setUiShowCachedFileCounts(final boolean uiShowCachedFileCounts) {
-        try {
-            setBooleanValue(uiShowCachedFileCounts, "uiShowCachedFileCounts");
-        } catch (InvalidPreferenceName e) {
-            _log.error("Invalid preference name 'uiShowCachedFileCounts': something is very wrong here.", e);
-        }
-    }
-    
-    @NrgPreference(defaultValue = "true")
     public boolean getUiShowPrearchiveFileActions() {
         return getBooleanValue("uiShowPrearchiveFileActions");
     }

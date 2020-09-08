9c7c69b7a8e5978d563043e01d5003f857a39143
Rick Herrick <jrherrick@wustl.edu>
2020-09-08 17:44:34 -0500

    XNAT-6452 Fixed issue where new users without project access caused massive stacktrace messages for no good reason

--- src/main/java/org/nrg/xdat/om/XdatFieldMapping.java
+++ src/main/java/org/nrg/xdat/om/XdatFieldMapping.java
@@ -9,50 +9,66 @@
 
 
 package org.nrg.xdat.om;
-import java.util.Hashtable;
 
+import org.apache.commons.lang3.StringUtils;
 import org.nrg.xdat.om.base.BaseXdatFieldMapping;
 import org.nrg.xft.ItemI;
 import org.nrg.xft.security.UserI;
 
+import java.util.Hashtable;
+import java.util.function.Predicate;
+
 /**
  * @author XDAT
- *
  */
 @SuppressWarnings("serial")
 public class XdatFieldMapping extends BaseXdatFieldMapping {
+    public XdatFieldMapping(ItemI item) {
+        super(item);
+    }
 
-	public XdatFieldMapping(ItemI item)
-	{
-		super(item);
+    public XdatFieldMapping(UserI user) {
+        super(user);
+    }
+
+    /*
+     * @deprecated Use BaseXdatFieldMapping(UserI user)
+     **/
+    public XdatFieldMapping() {
+    }
+
+    @SuppressWarnings("unused")
+	public XdatFieldMapping(Hashtable properties, UserI user) {
+        super(properties, user);
+    }
+
+    public void init(String psf, String value, Boolean create, Boolean read, Boolean delete, Boolean edit, Boolean activate) {
+        this.setField(psf);
+        this.setFieldValue(value);
+        this.setCreateElement(create);
+        this.setReadElement(read);
+        this.setEditElement(edit);
+        this.setDeleteElement(delete);
+        this.setActiveElement(activate);
+        this.setComparisonType("equals");
+    }
+
+    public static Filter filter(final String fieldName, final String fieldValue) {
+    	return new Filter(fieldName, fieldValue);
 	}
 
-	public XdatFieldMapping(UserI user)
-	{
-		super(user);
-	}
+    public static class Filter implements Predicate<XdatFieldMapping> {
+        Filter(final String fieldName, final String fieldValue) {
+            _fieldName = fieldName;
+            _fieldValue = fieldValue;
+        }
 
-	/*
-	 * @deprecated Use BaseXdatFieldMapping(UserI user)
-	 **/
-	public XdatFieldMapping()
-	{}
+        @Override
+        public boolean test(final XdatFieldMapping fieldMapping) {
+            return fieldMapping != null && StringUtils.equals(fieldMapping.getField(), _fieldName) && StringUtils.equals(fieldMapping.getFieldValue(), _fieldValue);
+        }
 
-	public XdatFieldMapping(Hashtable properties, UserI user)
-	{
-		super(properties,user);
-	}
-	
-	public void init(String psf,String value,Boolean create,Boolean read,Boolean delete,Boolean edit,Boolean activate){
-		this.setField(psf);
-		this.setFieldValue(value);
-
-		this.setCreateElement(create);
-		this.setReadElement(read);
-		this.setEditElement(edit);
-		this.setDeleteElement(delete);
-		this.setActiveElement(activate);
-		this.setComparisonType("equals");
-	}
-
+        private final String _fieldName;
+        private final String _fieldValue;
+    }
 }
